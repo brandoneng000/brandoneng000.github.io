010c9aa5b62b8e6806c9653a772760a56725ea13
Brandon Eng <brandoneng000@gmail.com>
2017-07-16 11:37:36 -0700

    modal open close

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,20 +1,41 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+function openModal(id) {
+   var modal = document.getElementById("picmodal" + id);
+   modal.style.opacity = 1;
+   modal.style.zIndex = 1000;
+   showSSpic(modal, 0);
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+   document.getElementById("dark-bg").style.display = "block";
+
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var currentSSIdx;
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
+function showSSpic(elem, idx) {
+    console.log("aosiuhd")
+    var slide = document.getElementById(elem.id + "-" + idx)
+    console.log(slide)
+    slide.style.display = "block";
+    currentSSIdx = idx;
+}
+
+// Get the modal
+var modal = document.getElementById('picmodal0');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    var modals = document.getElementsByClassName("picmodal");
+    for (var i = 0; i < modals.length; i++) {
+        if (event.target.className !== "picmodal" && 
+            event.target.className !== "button modalbutton" &&
+            event.target.className !== "fa fa-arrow-right" &&
+            event.target.className !== "fa fa-arrow-left" &&
+            event.target.className !== "picmodal-img" &&
+            modals[i].style.opacity === "1" ) {
+
+            modals[i].style.opacity = 0;
+            modals[i].style.zIndex = -1000;
+
+            document.getElementById("dark-bg").style.display = "none";
+        }
+    }
 }
